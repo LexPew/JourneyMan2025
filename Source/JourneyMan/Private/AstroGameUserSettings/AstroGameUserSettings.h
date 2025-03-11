acfab90efe69ca825c894f3c689639bebb8e8c86
snap <iofferyoutea@gmail.com>
2025-03-11 13:00:53 +0000

    Create custom game user settings class
    Extending from the built-in GameUserSettings class

---
+++ Source/JourneyMan/Private/AstroGameUserSettings/AstroGameUserSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "AstroGameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAstroGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+	
+};

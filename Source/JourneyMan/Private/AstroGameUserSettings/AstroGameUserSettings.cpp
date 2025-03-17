29c2d8489aded3df7dc246299625bcafdbd4face
snap <iofferyoutea@gmail.com>
2025-03-17 13:46:28 +0000

    Add some display, control, and sound settings to astro game user settings

--- Source/JourneyMan/Private/AstroGameUserSettings/AstroGameUserSettings.cpp
+++ Source/JourneyMan/Private/AstroGameUserSettings/AstroGameUserSettings.cpp
@@ -8,6 +8,31 @@ int32 UAstroGameUserSettings::GetDisplayID() const
 	return DisplayID;
 }
 
+void UAstroGameUserSettings::SetDisplayID(int32 NewDisplayID)
+{
+	DisplayID = NewDisplayID;
+}
+
+FVector2D UAstroGameUserSettings::GetMouseSensitivity() const
+{
+	return MouseSensitivity;
+}
+
+void UAstroGameUserSettings::SetMouseSensitivity(FVector2D NewMouseSensitivity)
+{
+	MouseSensitivity = NewMouseSensitivity;
+}
+
+int32 UAstroGameUserSettings::GetMasterVolume() const
+{
+	return MasterVolume;
+}
+
+void UAstroGameUserSettings::SetMasterVolume(int32 NewMasterVolume)
+{
+	MasterVolume = NewMasterVolume;
+}
+
 UAstroGameUserSettings* UAstroGameUserSettings::GetAstroGameUserSettings()
 {
 	return Cast<UAstroGameUserSettings>(GEngine->GetGameUserSettings());
--- Source/JourneyMan/Private/AstroGameUserSettings/AstroGameUserSettings.h
+++ Source/JourneyMan/Private/AstroGameUserSettings/AstroGameUserSettings.h
@@ -13,14 +13,43 @@ UCLASS(config = GameUserSettings, configdonotcheckdefaults, MinimalAPI)
 class UAstroGameUserSettings : public UGameUserSettings
 {
 	GENERATED_BODY()
-	
-public:
+
+private:
+	// Display
 	UPROPERTY(config)
 	int32 DisplayID;
 
+	// Control
+	UPROPERTY(config)
+	FVector2D MouseSensitivity;
+
+	// Sound
+	UPROPERTY(config)
+	int32 MasterVolume;
+
+	// Accessibility
+
+public:
+	// User-facing
+	// Display
 	UFUNCTION(BlueprintCallable, Category = "Settings|Config|Display")
 	int32 GetDisplayID() const;
+	UFUNCTION(BlueprintCallable, Category = "Settings|Config|Display")
+	void SetDisplayID(int32 NewDisplayID);
+	
+	// Control
+	UFUNCTION(BlueprintCallable, Category = "Settings|Config|Control")
+	FVector2D GetMouseSensitivity() const;
+	UFUNCTION(BlueprintCallable, Category = "Settings|Config|Control")
+	void SetMouseSensitivity(FVector2D NewMouseSensitivity);
+	
+	// Sound
+	UFUNCTION(BlueprintCallable, Category = "Settings|Config|Sound")
+	int32 GetMasterVolume() const;
+	UFUNCTION(BlueprintCallable, Category = "Settings|Config|Sound")
+	void SetMasterVolume(int32 NewMasterVolume);
 
+	// Internal
 	UFUNCTION(BlueprintCallable, Category = "Settings")
 	static UAstroGameUserSettings* GetAstroGameUserSettings();
 };
